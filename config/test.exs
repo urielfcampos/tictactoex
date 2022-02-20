f707c704885d3b6adaf8443df5efd3cc73409f30
Uriel Campos <urielcampos95@gmail.com>
2022-02-20 11:00:19 -0300

    add posgres container for tests

--- config/test.exs
+++ config/test.exs
@@ -7,8 +7,8 @@ import Config
 # Run `mix help test` for more information.
 config :tictactoex, Tictactoex.Repo,
   username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  password: "root",
+  hostname: System.get_env("POSTGRES_HOST"),
   database: "tictactoex_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
